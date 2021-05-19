26abb308e9a36d2f64c365cf3d15d144dc703e69
8locapeesh <84409005+8locapeesh@users.noreply.github.com>
2021-05-19 17:29:22 +0200

    Update Cell.java

--- agario/src/main/java/objects/Cell.java
+++ agario/src/main/java/objects/Cell.java
@@ -5,18 +5,18 @@ import board.MapGenerator;
 
 import java.awt.*;
 
-public class Cell  {
-    private int radius;
-    private int x;
-    private int y;
+public class Cell {
+    private double radius;
+    private double x;
+    private double y;
     private double mass;
-    private int deltaX;
-    private int deltaY;
+    private double deltaX;
+    private double deltaY;
     protected Color color;
-    protected int speed;
+    protected double speed;
 
 
-    public Cell(int radius, Color color, int x, int y, int deltaX, int deltaY, double mass){
+    public Cell(int radius, Color color, int x, int y, int deltaX, int deltaY, double mass, Color color, double speed) {
         this.radius = radius;
         this.color = color;
         this.x = x;
@@ -24,29 +24,65 @@ public class Cell  {
         this.deltaX = deltaX;
         this.deltaY = deltaY;
         this.mass = mass;
+        this.speed = speed;
+
     }
 
-    public int getX() {
+    public double getX() {
         return x;
     }
 
-
-    public int getY() {
+    public double getY() {
         return y;
     }
 
-    public int getDeltaX() {
+    public double getDeltaX() {
         return deltaX;
     }
 
-    public int getDeltaY() {
+    public double getDeltaY() {
         return deltaY;
     }
 
-    public int getRadius() {
+    public double getRadius() {
         return radius;
     }
-    public double getMass(){
-        return mass;
+
+    public double getMass() { return mass; }
+
+    public Color getColor() { return color; }
+
+    public double getSpeed() { return speed; }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setDeltaX(double deltaX) {
+        this.deltaX = deltaX;
+    }
+
+    public void setDeltaY(double deltaY) {
+        this.deltaY = deltaY;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
     }
 }
